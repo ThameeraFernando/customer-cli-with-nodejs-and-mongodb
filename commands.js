62c8eef98dcaabf3a5d45fe4e5b81eb37dcc7d32
ThameeraFernando <sithnuwan111@gmail.com>
2023-07-17 19:05:43 +0530

    add remaining crud

--- commands.js
+++ commands.js
@@ -1,5 +1,11 @@
 const program = require("commander");
-const { addCustomer, findCustomer } = require("./index");
+const {
+  addCustomer,
+  findCustomer,
+  updateCustomer,
+  removeCustomer,
+  listCustomer,
+} = require("./index");
 const { prompt } = require("inquirer");
 
 //customer questions
@@ -20,6 +26,7 @@ program.version("1.0.0").description("Client Management System");
 //     addCustomer({ firstName, lastName, phone, email });
 //   });
 
+//Find command
 program
   .command("find <name>")
   .alias("f")
@@ -28,12 +35,40 @@ program
     findCustomer(name);
   });
 
+//Add command
 program
   .command("add")
   .alias("a")
   .description("Add a customer")
   .action(() => {
     prompt(questions).then((answers) => addCustomer(answers));
+  });
+
+//Update command
+program
+  .command("update <_id>")
+  .alias("u")
+  .description("Update a customer")
+  .action((_id) => {
+    prompt(questions).then((answers) => updateCustomer(_id, answers));
+  });
+
+//Remove command
+program
+  .command("remove <_id>")
+  .alias("r")
+  .description("Remove a customer")
+  .action((_id) => {
+    removeCustomer(_id);
+  });
+
+//List command
+program
+  .command("list")
+  .alias("l")
+  .description("List all customers")
+  .action(() => {
+    listCustomer();
   });
 
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -20,13 +20,45 @@ const addCustomer = (customer) => {
 const findCustomer = (name) => {
   //Make case insensitive
   const search = new RegExp(name, "i");
-  Customer.find({ $or: [{ firstName: search }, { lastName: search }] }).then(
-    (customer) => {
+  Customer.find({ $or: [{ firstName: search }, { lastName: search }] })
+    .lean()
+    .then((customer) => {
       console.info(customer);
       console.info(`${customer.length} matches`);
       db.close();
-    }
-  );
+    });
+};
+// Update customer
+const updateCustomer = (_id, customer) => {
+  Customer.update({ _id }, customer).then((customer) => {
+    console.info("Customer Updated");
+    db.close();
+  });
 };
 
-module.exports = { addCustomer, findCustomer };
+// Remove customer
+const removeCustomer = (_id) => {
+  Customer.remove({ _id }).then((customer) => {
+    console.info("Customer Removed");
+    db.close();
+  });
+};
+
+//List Customers
+const listCustomer = () => {
+  Customer.find()
+    .lean()
+    .then((customers) => {
+      console.info(customers);
+      console.info(`${customers.length} customers`);
+      db.close();
+    });
+};
+
+module.exports = {
+  addCustomer,
+  findCustomer,
+  updateCustomer,
+  removeCustomer,
+  listCustomer,
+};
